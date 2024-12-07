d72fbd759cafa28d44031b6f66151f5ae3b61316
Kristian <10221425@ucn.dk>
2024-12-07 06:46:45 +0100

    Storage Buckets & Image Upload

--- app/actions/createRoom.js
+++ app/actions/createRoom.js
@@ -1,20 +1,41 @@
-'use server';
-import { createAdminClient } from '@/config/appwrite';
-import checkAuth from './checkAuth';
-import { ID } from 'node-appwrite';
-import { revalidatePath } from 'next/cache';
+"use server";
+import { createAdminClient } from "@/config/appwrite";
+import checkAuth from "./checkAuth";
+import { ID } from "node-appwrite";
+import { revalidatePath } from "next/cache";
 
 async function createRoom(previousState, formData) {
   // Get databases instance
-  const { databases } = await createAdminClient();
+  const { databases, storage } = await createAdminClient();
 
   try {
     const { user } = await checkAuth();
 
     if (!user) {
       return {
-        error: 'You must be logged in to create a room',
+        error: "You must be logged in to create a room",
       };
+    }
+
+    // Uploading image
+    let imageID;
+
+    const image = formData.get("image");
+
+    if (image && image.size > 0 && image.name !== "undefined") {
+      try {
+        // Upload
+        const response = await storage.createFile("rooms", ID.unique(), image);
+        // Save imageID in the database
+        imageID = response.$id;
+      } catch (error) {
+        console.log("Error uploading image", error);
+        return {
+          error: "Error uploading image",
+        };
+      }
+    } else {
+      console.log("No image file provided or file is invalid");
     }
 
     // Create room
@@ -24,27 +45,27 @@ async function createRoom(previousState, formData) {
       ID.unique(),
       {
         user_id: user.id,
-        name: formData.get('name'),
-        description: formData.get('description'),
-        sqft: formData.get('sqft'),
-        capacity: formData.get('capacity'),
-        location: formData.get('location'),
-        address: formData.get('address'),
-        availability: formData.get('availability'),
-        price_per_hour: formData.get('price_per_hour'),
-        amenities: formData.get('amenities'),
+        name: formData.get("name"),
+        description: formData.get("description"),
+        sqft: formData.get("sqft"),
+        capacity: formData.get("capacity"),
+        location: formData.get("location"),
+        address: formData.get("address"),
+        availability: formData.get("availability"),
+        price_per_hour: formData.get("price_per_hour"),
+        amenities: formData.get("amenities"),
+        image: imageID,
       }
     );
 
-    revalidatePath('/', 'layout');
+    revalidatePath("/", "layout");
 
     return {
       success: true,
     };
   } catch (error) {
     console.log(error);
-    const errorMessage =
-      error.response.message || 'An unexpected error has occured';
+    const errorMessage = error.response.message || "An unexpected error has occured";
     return {
       error: errorMessage,
     };
--- components/RoomCard.jsx
+++ components/RoomCard.jsx
@@ -1,12 +1,19 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 const RoomCard = ({ room }) => {
+  const bucketId = process.env.NEXT_PUBLIC_APPWRITE_STORAGE_BUCKET_ROOMS;
+  const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT;
+
+  const imageUrl = `https://cloud.appwrite.io/v1/storage/buckets/${bucketId}/files/${room.image}/view?project=${projectId}`;
+
+  const imageSrc = room.image ? imageUrl : "/images/no-image.jpg";
+
   return (
     <div className='bg-white shadow rounded-lg p-4 mt-4 flex flex-col sm:flex-row justify-between items-start sm:items-center'>
       <div className='flex flex-col sm:flex-row sm:space-x-4'>
         <Image
-          src={`/images/rooms/${room.image}`}
+          src={imageSrc}
           width={400}
           height={100}
           alt={room.name}
@@ -15,18 +22,14 @@ const RoomCard = ({ room }) => {
         <div className='space-y-1'>
           <h4 className='text-lg font-semibold'>{room.name}</h4>
           <p className='text-sm text-gray-600'>
-            <span className='font-semibold text-gray-800'> Address:</span>{' '}
-            {room.address}
+            <span className='font-semibold text-gray-800'> Address:</span> {room.address}
           </p>
           <p className='text-sm text-gray-600'>
-            <span className='font-semibold text-gray-800'>
-              Availability:
-            </span>
+            <span className='font-semibold text-gray-800'>Availability:</span>
             {room.availability}
           </p>
           <p className='text-sm text-gray-600'>
-            <span className='font-semibold text-gray-800'> Price:</span>$
-            {room.price_per_hour}/hour
+            <span className='font-semibold text-gray-800'> Price:</span>${room.price_per_hour}/hour
           </p>
         </div>
       </div>
