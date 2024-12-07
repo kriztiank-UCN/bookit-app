4eff8be02283aa54f71f1c67388305d48d1b6bdd
Kristian <10221425@ucn.dk>
2024-12-07 06:57:51 +0100

    Single Room Page Image

--- app/rooms/[id]/page.jsx
+++ app/rooms/[id]/page.jsx
@@ -1,9 +1,9 @@
-import Heading from '@/components/Heading';
-import BookingForm from '@/components/BookingForm';
-import Image from 'next/image';
-import Link from 'next/link';
-import { FaChevronLeft } from 'react-icons/fa';
-import getSingleRoom from '@/app/actions/getSingleRoom';
+import Heading from "@/components/Heading";
+import BookingForm from "@/components/BookingForm";
+import Image from "next/image";
+import Link from "next/link";
+import { FaChevronLeft } from "react-icons/fa";
+import getSingleRoom from "@/app/actions/getSingleRoom";
 
 const RoomPage = async ({ params }) => {
   const { id } = params;
@@ -13,21 +13,25 @@ const RoomPage = async ({ params }) => {
     return <Heading title='Room Not Found' />;
   }
 
+  const bucketId = process.env.NEXT_PUBLIC_APPWRITE_STORAGE_BUCKET_ROOMS;
+  const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT;
+
+  const imageUrl = `https://cloud.appwrite.io/v1/storage/buckets/${bucketId}/files/${room.image}/view?project=${projectId}`;
+
+  const imageSrc = room.image ? imageUrl : "/images/no-image.jpg";
+
   return (
     <>
       <Heading title={room.name} />
       <div className='bg-white shadow rounded-lg p-6'>
-        <Link
-          href='/'
-          className='flex items-center text-gray-600 hover:text-gray-800 mb-4'
-        >
+        <Link href='/' className='flex items-center text-gray-600 hover:text-gray-800 mb-4'>
           <FaChevronLeft className='inline mr-1' />
           <span className='ml-2'>Back to Rooms</span>
         </Link>
 
         <div className='flex flex-col sm:flex-row sm:space-x-6'>
           <Image
-            src={`/images/rooms/${room.image}`}
+            src={imageSrc}
             alt={room.name}
             width={400}
             height={100}
@@ -39,23 +43,19 @@ const RoomPage = async ({ params }) => {
 
             <ul className='space-y-2'>
               <li>
-                <span className='font-semibold text-gray-800'>Size:</span>{' '}
-                {room.sqft}
+                <span className='font-semibold text-gray-800'>Size:</span> {room.sqft}
                 sq ft
               </li>
               <li>
-                <span className='font-semibold text-gray-800'>
-                  Availability:
-                </span>
+                <span className='font-semibold text-gray-800'>Availability:</span>
                 {room.availability}
               </li>
               <li>
-                <span className='font-semibold text-gray-800'>Price:</span>$
-                {room.price_per_hour}/hour
+                <span className='font-semibold text-gray-800'>Price:</span>${room.price_per_hour}
+                /hour
               </li>
               <li>
-                <span className='font-semibold text-gray-800'>Address:</span>{' '}
-                {room.address}
+                <span className='font-semibold text-gray-800'>Address:</span> {room.address}
               </li>
             </ul>
           </div>
