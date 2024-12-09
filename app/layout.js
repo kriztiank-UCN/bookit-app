08013dbb5861569a0fc435cb7ffc7d5819c6e1fb
Kristian <10221425@ucn.dk>
2024-12-09 09:58:22 +0100

    Admin Passcode Verification

--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,37 @@
-import { Inter } from 'next/font/google';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import AuthWrapper from '@/components/AuthWrapper';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import '@/assets/styles/globals.css';
+import { Plus_Jakarta_Sans } from "next/font/google";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import AuthWrapper from "@/components/AuthWrapper";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "@/assets/styles/globals.css";
 
-const inter = Inter({ subsets: ['latin'] });
+import { cn } from "@/lib/utils";
+
+const fontSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-sans",
+});
 
 export const metadata = {
-  title: 'Bookit App | Book a Room',
-  description: 'Book a meeting or conference room for your team',
+  title: "Bookit App | Book a Room",
+  description: "Book a meeting or conference room for your team",
 };
 
 export default function RootLayout({ children }) {
   return (
     <AuthWrapper>
       <html lang='en'>
-        <body className={inter.className}>
+        <body
+          // className={inter.className}
+          className={cn(
+            "min-h-screen bg-dark-500 font-sans antialiased",
+            fontSans.variable
+          )}
+        >
           <Header />
-          <main className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
-            {children}
-          </main>
+          <main className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>{children}</main>
           <Footer />
           <ToastContainer />
         </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,10 +12,44 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        green: {
+          500: "#24AE7C",
+          600: "#0D2A1F",
+        },
+        blue: {
+          500: "#79B5EC",
+          600: "#152432",
+        },
+        red: {
+          500: "#F37877",
+          600: "#3E1716",
+          700: "#F24E43",
+        },
+        light: {
+          200: "#E8E9E9",
+        },
+        dark: {
+          200: "#0D0F10",
+          300: "#131619",
+          400: "#1A1D21",
+          500: "#363A3D",
+          600: "#76828D",
+          700: "#ABB8C4",
+        },
+      },
+      fontFamily: {
+        sans: ["var(--font-sans)", ...fontFamily.sans],
+      },
+      keyframes: {
+        "caret-blink": {
+          "0%,70%,100%": { opacity: "1" },
+          "20%,50%": { opacity: "0" },
+        },
+      },
+      animation: {
+        "caret-blink": "caret-blink 1.25s ease-out infinite",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
