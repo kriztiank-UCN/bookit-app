3d3914bcb328aa74c5b4fd97e5aabf7d8f2d4a59
Kristian <10221425@ucn.dk>
2025-01-08 11:10:48 +0100

    Show Bookings

--- app/bookings/page.jsx
+++ app/bookings/page.jsx
@@ -1,9 +1,19 @@
-const BookingsPage = () => {
-  return (
-    <div>
-      Bookings
-    </div>
-  )
-}
+import Heading from "@/components/Heading";
+import getMyBookings from "../actions/getMyBookings";
 
-export default BookingsPage
+const BookingsPage = async () => {
+  const bookings = await getMyBookings();
+
+  return (
+    <>
+      <Heading title='My Bookings' />
+      {bookings.length === 0 ? (
+        <p className='text-gray-600 mt-4'>You have no bookings</p>
+      ) : (
+        bookings.map(booking => <h3 key={booking.$id}>{booking.room_id.name} </h3>)
+      )}
+    </>
+  );
+};
+
+export default BookingsPage;
