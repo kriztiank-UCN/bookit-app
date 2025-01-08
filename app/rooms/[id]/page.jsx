3660d61749e25c42060a3b3e4229d45483d73e16
Kristian <10221425@ucn.dk>
2025-01-08 10:18:54 +0100

    Book Room Action

--- app/rooms/[id]/page.jsx
+++ app/rooms/[id]/page.jsx
@@ -1,9 +1,9 @@
-import Heading from "@/components/Heading";
-import BookingForm from "@/components/BookingForm";
-import Image from "next/image";
-import Link from "next/link";
-import { FaChevronLeft } from "react-icons/fa";
-import getSingleRoom from "@/app/actions/getSingleRoom";
+import Heading from '@/components/Heading';
+import BookingForm from '@/components/BookingForm';
+import Image from 'next/image';
+import Link from 'next/link';
+import { FaChevronLeft } from 'react-icons/fa';
+import getSingleRoom from '@/app/actions/getSingleRoom';
 
 const RoomPage = async ({ params }) => {
   const { id } = params;
@@ -18,13 +18,16 @@ const RoomPage = async ({ params }) => {
 
   const imageUrl = `https://cloud.appwrite.io/v1/storage/buckets/${bucketId}/files/${room.image}/view?project=${projectId}`;
 
-  const imageSrc = room.image ? imageUrl : "/images/no-image.jpg";
+  const imageSrc = room.image ? imageUrl : '/images/no-image.jpg';
 
   return (
     <>
       <Heading title={room.name} />
       <div className='bg-white shadow rounded-lg p-6'>
-        <Link href='/' className='flex items-center text-gray-600 hover:text-gray-800 mb-4'>
+        <Link
+          href='/'
+          className='flex items-center text-gray-600 hover:text-gray-800 mb-4'
+        >
           <FaChevronLeft className='inline mr-1' />
           <span className='ml-2'>Back to Rooms</span>
         </Link>
@@ -43,25 +46,28 @@ const RoomPage = async ({ params }) => {
 
             <ul className='space-y-2'>
               <li>
-                <span className='font-semibold text-gray-800'>Size:</span> {room.sqft}
+                <span className='font-semibold text-gray-800'>Size:</span>{' '}
+                {room.sqft}
                 sq ft
               </li>
               <li>
-                <span className='font-semibold text-gray-800'>Availability:</span>
+                <span className='font-semibold text-gray-800'>
+                  Availability:
+                </span>
                 {room.availability}
               </li>
               <li>
-                <span className='font-semibold text-gray-800'>Price:</span>${room.price_per_hour}
-                /hour
+                <span className='font-semibold text-gray-800'>Price:</span>$
+                {room.price_per_hour}/hour
               </li>
               <li>
-                <span className='font-semibold text-gray-800'>Address:</span> {room.address}
+                <span className='font-semibold text-gray-800'>Address:</span>{' '}
+                {room.address}
               </li>
             </ul>
           </div>
         </div>
-
-        <BookingForm />
+        <BookingForm room={room} />
       </div>
     </>
   );
