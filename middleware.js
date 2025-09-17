2d2362541298e863187e82f65dd572ec6c1eacdb
Kristian <10221425@ucn.dk>
2025-09-17 11:20:47 +0200

    protect login & register routes, redirect to homepage

--- middleware.js
+++ middleware.js
@@ -1,18 +1,29 @@
+// This middleware checks if the user is authenticated before allowing access to certain routes.
 import { NextResponse } from "next/server";
 import checkAuth from "./app/actions/checkAuth";
 
 export async function middleware(request) {
-  // check if user is authenticated
+  // Get authentification status from checkAuth action
   const { isAuthenticated } = await checkAuth();
-
-  // if not authenticated, redirect to login page
+  // Extract the pathname from the request URL
+  const { pathname } = new URL(request.url);
+  // Protect /login and /register routes
+  if (pathname === "/login" || pathname === "/register") {
+    // If authenticated, redirect to homepage
+    if (isAuthenticated) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+    // If not authenticated, allow access
+    return NextResponse.next();
+  }
+  // For other protected routes, require authentication
   if (!isAuthenticated) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
-
+  // If authenticated, allow access
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/bookings", "/rooms/add", "/rooms/my"],
+  matcher: ["/bookings", "/rooms/add", "/rooms/my", "/login", "/register"],
 };
